1d19f54d6ee319e98619d3ab52fd6e8419c6503d
James Clarke <164679524+jjjclarke@users.noreply.github.com>
2024-07-22 11:45:30 +0100

    Added some documentation to Flags

--- src/where/Flags.java
+++ src/where/Flags.java
@@ -3,12 +3,33 @@ package where;
 public class Flags {
 
 	// toggleable options
+	
+	// Show the computer hostname (e.g. "john@arch")
+	// DEFAULT: true
 	public static boolean SHOW_HOSTNAME = true;
+	
+	// Show the CPU architecture (e.g. AMD Athlon Gold 3150U with Radeon Graphics)
+	// DEFAULT: false
 	public static boolean SHOW_CPU = false;
-	public static boolean SHOW_GPU = false;
+	
+	// Show the GPU (e.g. [AMD/ATI] Picasso/Raven 2) - not recommended to use
+	// DEFAULT: false
+	public static boolean SHOW_GPU = true;
+	
+	// Show the operating system in use (e.g. Arch Linux)
+	// DEFAULT: true
 	public static boolean SHOW_OS = true;
+	
+	// Show the current shell in use (e.g. bash)
+	// DEFAULT: true
 	public static boolean SHOW_SHELL = true;
+	
+	// Show the number of packages installed via package managers (e.g. 500)
+	// DEFAULT: true
 	public static boolean SHOW_PKGS = true;
+	
+	// Show the running Linux kernel version (e.g. 6.4.10.5-manjaro)
+	// DEFAULT: true
 	public static boolean SHOW_KERNEL = true;
 	
 }
